846076af8a1256d14454ec93906d395034ed794e
Nicolas <nmosso@gmail.com>
2018-07-20 07:28:42 -0400

    Variable de entorno Mongo URL_DB

--- server/config/config.js
+++ server/config/config.js
@@ -13,12 +13,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //DB
 
 let urlDB;
-/*
+
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://nmosso:02Daniel@ds143511.mlab.com:43511/cafe-nmosso';
+    urlDB = process.env.URL_DB;
 }
-*/
-urlDB = 'mongodb://nmosso:02Daniel@ds143511.mlab.com:43511/cafe-nmosso';
+/**/
+//urlDB = 'mongodb://nmosso:02Daniel@ds143511.mlab.com:43511/cafe-nmosso';
 process.env.URLDB = urlDB;
